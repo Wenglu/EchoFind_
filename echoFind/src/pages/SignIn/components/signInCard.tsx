e75a30cd1a0a582f45426dc78e320ff9ddfff421
Karol Węglarz <karolweglarz2003@gmail.com>
2026-04-24 12:31:33 +0200

    Revert "Merge branch 'Login' into Dashboard"
    This reverts commit 8bfaabcf5ad766c57138cf2d605565c652560e13, reversing
    changes made to a6d25515657522ce0eb2e1682250bf4c7d015490.

--- echoFind/src/pages/SignIn/components/signInCard.tsx
+++ echoFind/src/pages/SignIn/components/signInCard.tsx
@@ -13,6 +13,13 @@ import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
 import ForgotPassword from "./forgotPassword";
 import { useNavigate } from "react-router-dom";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../../../firebase/firebase";
 
 const Card = styled(MuiCard)(({ theme }) => ({
   display: "flex",
